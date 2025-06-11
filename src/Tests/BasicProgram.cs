1c28eb6d105484150c474510e57f22ffbebb3bde
Logan Bussell <github@loganbussell.com>
2025-06-11 15:52:21 -0700

    Add failing test

---
+++ src/Tests/BasicProgram.cs
@@ -0,0 +1,26 @@
+namespace NesNes.Tests;
+
+using NesNes.Core;
+
+public class BasicProgram
+{
+    [Fact]
+    public void Test1()
+    {
+        byte[] program =
+        [
+            0xA9, // LDA $C0
+            0xC0,
+            0xAA, // TAX
+            0xE8, // INX
+            0x00  // BRK
+        ];
+
+        var cpu = new Cpu();
+        cpu.Run(program);
+
+        // Pass the test if the program did not crash.
+        // TODO: validate the final state of the CPU
+        Assert.True(true);
+    }
+}
