1d883adb4af32f43196a69321f5afae2ab672c9b
Logan Bussell <github@loganbussell.com>
2025-06-19 21:45:09 -0700

    Add nestest test stub

---
+++ src/Tests/NesTest.cs
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 Logan Bussell
+// SPDX-License-Identifier: MIT
+
+using NesNes.Core;
+using Shouldly;
+
+namespace NesNes.Tests;
+
+public class NesTest
+{
+    // load nestest.nes
+    // "automated mode" - start execution at 0xC000
+    // compare traces to nestest.log.txt
+}
