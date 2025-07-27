486a97ea6ae6be0d74b06e633ce9193f0fa2c82c
Logan Bussell <github@loganbussell.com>
2025-07-27 09:32:01 -0700

    Remove MonoGame project

--- src/Host/IBuffered.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: Copyright (c) 2025 Logan Bussell
-// SPDX-License-Identifier: MIT
-
-namespace NesNes.Host;
-
-internal interface IBuffered
-{
-    void Render();
-}
--- src/Host/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// SPDX-FileCopyrightText: Copyright (c) 2025 Logan Bussell
-// SPDX-License-Identifier: MIT
-
-using ConsoleAppFramework;
-
-var app = ConsoleApp.Create();
-app.Add<EmulatorCli>();
-await app.RunAsync(args);
